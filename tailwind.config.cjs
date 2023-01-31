3e8c94cba46b7245c38db289e8f7572ac4536b42
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2023-01-31 07:46:03 +0100

    Completed todoApp in typescript

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/main.tsx",
+    "./src/App.tsx",
+    "./src/Container.tsx",
+    "./src/NewTask.tsx",
+    "./src/EditTask.tsx",
+    "./src/AllTasks.tsx",
+    "./src/CompletedTasks.tsx",
+    "./src/PendingTasks.tsx",
+    "./src/components/Header.tsx",
+    "./src/components/Nav.tsx",
+    "./src/components/Task.tsx",
+    "./src/components/EditAndDeleteButton.tsx",
+    "./src/components/Footer.tsx",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
